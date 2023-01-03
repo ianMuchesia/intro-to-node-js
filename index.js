0af9f826209ecc6b2b9eeb65418175d0d0cd3096
ianMuchesia <ianmuchesia@yahoo.com>
2023-01-03 13:06:29 +0300

    lesson_1

---
+++ index.js
@@ -0,0 +1,14 @@
+/* require("./batman")
+require("./superman") */
+/* const superHero = require("./superHero")
+
+console.log(superHero.getName());
+superHero.setName("SuperMan");
+console.log(superHero.getName());
+
+const newSuperHero = require("./superHero")
+console.log(newSuperHero.getName()); */
+
+const {add, sub}= require("./math")
+
+console.log(add(5,6));
---
+++ lesson_1.js
@@ -0,0 +1,6 @@
+const add  = require("./add")
+console.log("hello from index.js")
+const sum = add(1,2)
+const sum_2 = add(3,4)
+console.log(sum);
+console.log(sum_2);
